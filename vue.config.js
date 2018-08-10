79517dbcaf740f07bad6fc19a18ba36e1e9fdd24
gtalarico <gtalarico@gmail.com>
2018-08-10 02:19:21 -0400

    Updated Vue config notes

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,19 @@
-const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+// const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 
 module.exports = {
     outputDir: 'dist',
     assetsDir: 'static',
     // baseUrl: IS_PRODUCTION
-    // For Production, replace Static Base Url with Static Server / CDN
-    // And then point your CDN to yourdomain.com/static
-    // Use Whitenoise to serve assets at /static
-    // and have your cdn cache and distribute
-      // ? 'http://cdn123.com' // eg. www.cdn123.com/
-      // : '/',
+    // ? 'http://cdn123.com'
+    // : '/',
+    // For Production, replace set baseUrl to CDN
+    // And set the CDN origin to `yourdomain.com/static`
+    // Whitenoise will serve once to CDN which will then cache
+    // and distribute
     devServer: {
       proxy: {
         '/api*': {
-          // Forward frontend dev server request for /api to django devserver
+          // Forward frontend dev server request for /api to django dev server
           target: 'http://localhost:8000/',
         }
       }
