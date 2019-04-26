7fba8854a8c50e4adb56db94a88aef4a2025d7d9
kevinqqnj <ezhqing@gmail.com>
2019-04-26 13:26:43 +0800

    link

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <img src='@/assets/logo-django.png' style="width: 250px" />
+    <p><a href="https://github.com/kevinqqnj/django-vue-template"
+        target="_blank"
+        rel="noopener">Source</a>
+    </p>
     <RestMessage />
   </div>
 </template>
