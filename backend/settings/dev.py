55f9cbd7705081f4ff22aa1f970a4c84dd251c3b
kevinqqnj <ezhqing@gmail.com>
2019-04-26 13:42:30 +0800

    host

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'verybadsecret!!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
