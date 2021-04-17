e2d330b9a863dc942e03b7f4fc2b6b686f57876d
YCWangVince <wyc2016@mail.ustc.edu.cn>
2021-04-17 17:57:35 +0800

    login

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from '@/App.vue'
 
-import store from '@/store' 
+import store from '@/store'
 import router from '@/router'
-
+import ElementUI from "element-ui";
+import './plugins/element.js'
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 // Vue.use(VueRouter)
