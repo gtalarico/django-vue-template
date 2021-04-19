f253e30d59e57ee93c11f1780a0c80489fec2b10
vermouthdky <1048180828@qq.com>
2021-04-19 20:05:44 +0800

    iteration 1

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,7 +2,7 @@
 
 import { register } from "register-service-worker";
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "none") {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
       console.log(
