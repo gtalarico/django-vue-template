29f966bb6b7425d2bdbe27b22d9bf09689f78a27
Mooler0410 <814030466@qq.com>
2021-04-26 20:41:46 +0800

    add dj-heroku to dev.py

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 SETTINGS_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -172,3 +173,5 @@ CORS_ALLOW_HEADERS = (
     'Pragma',
     'id',
 )
+
+django_heroku.settings(locals())
--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 import os
 import sys
 
+
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.dev')
     try:
@@ -12,4 +13,6 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
+    
