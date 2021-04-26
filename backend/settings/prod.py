53c63a8490cbb4291074369e94d61722319e69b0
Mooler0410 <814030466@qq.com>
2021-04-26 20:31:37 +0800

    django_heroku

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -2,6 +2,8 @@
 
 import os
 import dj_database_url
+import django_heroku
+
 from .dev import *
 
 ############
@@ -22,5 +24,8 @@ DEBUG = bool(os.getenv('DJANGO_DEBUG', ''))
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
 
+
 # Set to your Domain here (eg. 'django-vue-template-demo.herokuapp.com')
 ALLOWED_HOSTS = ['portfoliotradingassistant.herokuapp.com', 'localhost:8000', 'localhost:8080']
+
+django_heroku.settings(locals())
