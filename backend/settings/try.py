dd0b58a6342d50767effe68ddc7f1de4290b1c4f
Mooler0410 <814030466@qq.com>
2021-05-02 07:58:28 +0800

    delete try

--- backend/settings/try.py
+++
@@ -1,8 +0,0 @@
-if True:
-    mail_tempelate = 'Dear user:\n\
-        Your stock has reached a price of . \
-    Th e return from holding the security until , assuming it reached a price target of, would be. \
-    At the recent market price of , if you sell and invest the proceeds at your expected rate of return, \
-    your return from selling now is . Therefore, we advise selling now.'
-
-print(mail_tempelate)
